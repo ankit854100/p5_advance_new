0bab8ea937c3559c71ecfe4dac2b274de0416f8d
Your Name <you@example.com>
2021-06-07 20:46:35 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Workspace from "./Workspace";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+let html = require("../docs/iFrame.html");
+let css = require("../docs/iFrame.css");
+
+export default function App() {
+  const [srcDoc, setSrcDoc] = useState("");
+  const [WorkspaceStatus, setStatus] = useState("start");
+
+      // `<html><body>${html}</body><style>${css}</style><script>${code}</script></html>`
+  function frameInject(code) {
+    setStatus("restart");
+    setSrcDoc(
+      `<html><body>${html}</body><script>${code}</script></html>`
+    );
+  }
+  return (
+    <React.Fragment>
+      <div id="wrapper">
+        <div className="blocklyContainer">
+          <Workspace status={WorkspaceStatus} setSrc={frameInject} />
+        </div>
+        <div className="iframeContainer">
+          <iframe
+            id="blocklyIframe"
+            srcDoc={srcDoc}
+            title="output"
+            sandbox="allow-scripts allow-same-origin"
+            allow="fullscreen"
+            frameBorder="0"
+            style={{ display: "inline" }}
+          ></iframe>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+//  <div className="blocklyContainer">
+//         <Workspace status={WorkspaceStatus} setSrc={frameInject} />
+//       </div>
+//       <div className="iframeContainer">
+//         <iframe
+//           id="blocklyIframe"
+//           srcDoc={srcDoc}
+//           title="output"
+//           sandbox="allow-scripts allow-same-origin"
+//           allow="fullscreen"
+//           frameBorder="0"
+//           style={{ display: "inline" }}
+//         ></iframe>
+//       </div>
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          type: 'asset/resource',
+          generator: {
+            filename: '[name][ext]',
+          },
+        },
+        {
+          test: /\.html$/i,
+          use: ['extract-loader', 'html-loader'],
+        },
+        {
+          test: /\.js$/i,
+          exclude: /\.file.js$/i,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.file.js$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.css$/i,
+          exclude: /\.file.css$/i,
+          loader: 'css-loader',
+        },
+        {
+          test: /\.file.css$/i,
+          type: 'asset/resource',
+        },
+      ],
+    },
+  };
+  Te
